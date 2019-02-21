303d1116e5e59c0ceaea790e01edbcab63ac8e2a
Christopher da Rosa Michel <christopher.r.michel@gmail.com>
2019-02-21 19:39:33 -0300

    FEAT(Domain) - Criar biblioteca de classes e organizar estrutura classes.

---
+++ Switch.Domain/Entities/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using Switch.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Switch.Domain.Entities
+{
+    public class Usuario
+    {
+        public int Id { get; private set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string UrlFoto { get; set; }
+        public SexoEnum Sexo { get; set; }
+    }
+}
---
+++ Switch.Domain/Enums/SexoEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Switch.Domain.Enums
+{
+    public enum SexoEnum
+    {
+        Feminino = 1,
+        Masculino = 2,
+        NaoBinario = 3
+    }
+}
